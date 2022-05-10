079f27023d63faac63613eb9845a5ce113cac458
Martin <martin@flesk.es>
2022-05-10 10:25:07 +0200

    fixed login links and changed default active tab

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,44 +1,54 @@
-import "../styles/Login.css";
-import React, { useState, useContext } from "react";
-import { Navigate, useLocation } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
+import '../styles/Login.css'
+import React, { useState, useContext } from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
 
 function Login() {
-  const [userInput, setUserInput] = useState({ username: "", password: "" });
-  const { login, loggedIn } = useContext(AuthContext);
-  const { state } = useLocation();
-  const nextPage = state?.nextPage;
+  const [userInput, setUserInput] = useState({ username: '', password: '' })
+  const { login, loggedIn } = useContext(AuthContext)
+  const { state } = useLocation()
+  const nextPage = state?.nextPage
+
   const handleLogin = (e) => {
-    e.preventDefault();
-    console.log(state);
-    login(userInput, nextPage);
-  };
-  console.log("loggedIn aus Context", loggedIn);
-  if (loggedIn) return <Navigate to={nextPage ? nextPage : "/profile"} />;
+    e.preventDefault()
+    console.log(state)
+    login(userInput, nextPage)
+  }
+
+  if (loggedIn) return <Navigate to={nextPage ? nextPage : '/profile'} />
+
   return (
-    <div className="row">
-      <div className="col-md-6 mx-auto p-0">
-        <div className="card">
-          <div className="login-box">
-            <div className="login-snip">
-              <input id="tab-1" type="radio" name="tab" className="sign-in" />
-              <label className="tab">Login</label>
-              <input id="tab-2" type="radio" name="tab" className="sign-up" />
-              <label htmlFor="tab-2" className="tab">
+    <div className='row'>
+      <div className='col-md-6 mx-auto p-0'>
+        <div className='card'>
+          <div className='login-box'>
+            <div className='login-snip'>
+              <input
+                id='tab-1'
+                type='radio'
+                name='tab'
+                className='sign-in'
+                checked
+              />
+              <label htmlFor='tab-1' className='tab'>
+                Login
+              </label>
+              <input id='tab-2' type='radio' name='tab' className='sign-up' />
+              <label htmlFor='tab-2' className='tab'>
                 Sign Up
               </label>
-              <div className="login-space">
-                <div className="login">
-                  <div className="group">
-                    <label htmlFor="user" className="label">
+              <div className='login-space'>
+                <div className='login'>
+                  <div className='group'>
+                    <label htmlFor='user' className='label'>
                       Username
                     </label>
                     <input
-                      id="user"
-                      type="text"
-                      className="input"
-                      name="username"
-                      placeholder="Enter your username"
+                      id='user'
+                      type='text'
+                      className='input'
+                      name='username'
+                      placeholder='Enter your username'
                       onChange={(e) =>
                         setUserInput({
                           username: e.target.value,
@@ -47,17 +57,17 @@ function Login() {
                       }
                     />
                   </div>
-                  <div className="group">
-                    <label htmlFor="pass" className="label">
+                  <div className='group'>
+                    <label htmlFor='pass' className='label'>
                       Password
                     </label>
                     <input
-                      id="pass"
-                      type="password"
-                      name="password"
-                      className="input"
-                      data-type="password"
-                      placeholder="Enter your password"
+                      id='pass'
+                      type='password'
+                      name='password'
+                      className='input'
+                      data-type='password'
+                      placeholder='Enter your password'
                       onChange={(e) =>
                         setUserInput({
                           username: userInput.username,
@@ -66,40 +76,40 @@ function Login() {
                       }
                     />
                   </div>
-                  <div className="group">
+                  <div className='group'>
                     <input
-                      id="check"
-                      type="checkbox"
-                      className="check"
+                      id='check'
+                      type='checkbox'
+                      className='check'
                       checked
                     />
-                    <label htmlFor="check">
-                      <span className="icon"></span> Keep me Signed in
+                    <label htmlFor='check'>
+                      <span className='icon'></span> Keep me Signed in
                     </label>
                   </div>
-                  <div className="group">
+                  <div className='group'>
                     <input
-                      type="submit"
-                      className="button"
-                      value="Sign In"
+                      type='submit'
+                      className='button'
+                      value='Sign In'
                       onClick={handleLogin}
                     />
                   </div>
-                  <div className="hr"></div>
-                  <div className="foot">
-                    <a href="/">Forgot Password?</a>
+                  <div className='hr'></div>
+                  <div className='foot'>
+                    <a href='/'>Forgot Password?</a>
                   </div>
                 </div>
-                <div className="sign-up-form">
-                  <div className="group">
-                    <label htmlFor="user" className="label">
+                <div className='sign-up-form'>
+                  <div className='group'>
+                    <label htmlFor='user' className='label'>
                       Username
                     </label>
                     <input
-                      id="user"
-                      type="text"
-                      className="input"
-                      placeholder="Create your Username"
+                      id='user'
+                      type='text'
+                      className='input'
+                      placeholder='Create your Username'
                       onChange={(e) =>
                         setUserInput({
                           name: e.target.value,
@@ -108,16 +118,16 @@ function Login() {
                       }
                     />
                   </div>
-                  <div className="group">
-                    <label htmlFor="pass" className="label">
+                  <div className='group'>
+                    <label htmlFor='pass' className='label'>
                       Password
                     </label>
                     <input
-                      id="pass"
-                      type="password"
-                      className="input"
-                      data-type="password"
-                      placeholder="Create your password"
+                      id='pass'
+                      type='password'
+                      className='input'
+                      data-type='password'
+                      placeholder='Create your password'
                       onChange={(e) =>
                         setUserInput({
                           name: e.target.value,
@@ -127,17 +137,17 @@ function Login() {
                     />
                   </div>
 
-                  <div className="group">
+                  <div className='group'>
                     <input
-                      type="submit"
-                      className="button"
-                      value="Sign Up"
+                      type='submit'
+                      className='button'
+                      value='Sign Up'
                       onClick={handleLogin}
                     />
                   </div>
-                  <div className="hr"></div>
-                  <div className="foot">
-                    <label htmlFor="tab-1">Already Member?</label>
+                  <div className='hr'></div>
+                  <div className='foot'>
+                    <label htmlFor='tab-1'>Already Member?</label>
                   </div>
                 </div>
               </div>
@@ -146,7 +156,7 @@ function Login() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
